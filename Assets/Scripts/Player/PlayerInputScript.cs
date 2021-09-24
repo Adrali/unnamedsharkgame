efc7d99d81230f3257cdb1a2fc109046659f35b2
Sylvain Rocland <56031976+Riggor-Morthis@users.noreply.github.com>
2021-09-24 08:39:53 +0200

    Fin du debut
    La base de l'aspect platformer est présente, hésitez pas à fabriquer votre niveau de test et à me dire ce que vous en pensez

--- Assets/Scripts/Player/PlayerInputScript.cs
+++ Assets/Scripts/Player/PlayerInputScript.cs
@@ -4,22 +4,36 @@ using UnityEngine;
 
 public class PlayerInputScript : MonoBehaviour
 {
-    PlayerMovementScript playerMover;
-    float xMovement;
-    bool jump;
+    //Privates
+    private PlayerMovementScript playerMover;
+    private float xMovement;
+    private bool jump_i;
+    private bool jump_c;
 
+    //Initialisation : on a juste besoin de recuperer un script
     void Start()
     {
        playerMover = GetComponent<PlayerMovementScript>();
     }
 
+    //On awake sans valeur dans les inputs, pour eviter toute erreur
+    private void Awake()
+    {
+        xMovement = 0f;
+        jump_i = false;
+        jump_c = false;
+    }
+
+    //On passe les updates a recuperer les inputs, d'ou l'interet de scripts separes :
+    //Les updates ne seront que ici, et les FixedUpdates dans les autres scripts joueurs
     void Update()
     {
         //On recupère les inputs
         xMovement = Input.GetAxisRaw("Horizontal");
-        jump = Input.GetButtonDown("Jump");
+        jump_i = Input.GetButtonDown("Jump");
+        jump_c = Input.GetButton("Jump");
 
         //On les envoit au joueur
-        playerMover.ReceiveInputs(xMovement, jump);
+        playerMover.ReceiveInputs(xMovement, jump_i, jump_c);
     }
 }
