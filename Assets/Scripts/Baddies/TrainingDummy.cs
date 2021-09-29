dcaeb4f2a2eb3c488756f99417fe542cf7721579
Sylvain Rocland <56031976+Riggor-Morthis@users.noreply.github.com>
2021-09-29 11:55:48 +0200

    Combat Mario 1.0
    Les bases du combat mario, très peu fonctionnel pour le moment...

---
+++ Assets/Scripts/Baddies/TrainingDummy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingDummy : MonoBehaviour, IDamageable
+{
+    //Privates
+    private int healthPoints = 3;
+    private GameObject theDummy;
+
+    private void Awake()
+    {
+        theDummy = gameObject;
+    }
+
+    public void TakeDamage(int damages)
+    {
+        healthPoints -= damages;
+    }
+
+    void OnDeath()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerCombatScript.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombatScript : APlayer, IDamageable
+{
+    //Constantes
+    private const int bootDamage = 5; //Dégâts infligés lorsqu'on aterrit sur un ennemi
+    private const float bootReach = 1.6f; //La "portée" de nos bottes pour toucher les ennemis directement en dessous de nous
+
+    //Privates
+    private Rigidbody2D playerRigidbody; //Le rigidbody de notre personnage, pour le faire bouger
+    private int healthPoints = 1; //Le nombre de points de vie dispo pour notre perso
+    private RaycastHit2D baddieHit; //Pour stocker les ennemis touches par un raycast
+    private IDamageable baddieBody; //Pour stocker le component IDamageable du truc touche
+
+    //Publique
+    public LayerMask baddiesLayer; //Le layer utilisé pour les gens sur lesquels on peut taper
+
+    //Première méthode lancé
+    private void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    //Fait pour les calculs physiques
+    private void FixedUpdate()
+    {
+        
+    }
+
+    /// <summary>
+    /// Utilise pour infliger des dégâts aux ennemis lorsque le joueur atterit dessus
+    /// </summary>
+    private void BootOnHead()
+    {
+        //On doit être en l'air, mais surtout en chute libre
+        if(!isGrounded && !isDashing && !isJumping)
+        {
+            //On (essaye de) récupére(r) ce qu'on touche avec nos bottes
+            baddieHit = Physics2D.Raycast(playerRigidbody.position, Vector2.down, bootReach, baddiesLayer);
+            if(baddieHit.collider != null)
+            {
+                //Si le truc récupéré implémente l'interface IDamageable, on lui fait des dégâts
+                baddieBody = baddieHit.collider.gameObject.GetComponent<IDamageable>();
+                if (baddieBody != null) baddieBody.TakeDamage(bootDamage);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Comment infliger des dégâts à notre personnage
+    /// </summary>
+    /// <param name="damages">Quantité de dégâts à infliger</param>
+    public void TakeDamage(int damages)
+    {
+        healthPoints -= damages;
+        if (healthPoints <= 0) OnDeath();
+    }
+
+    /// <summary>
+    /// Quoi faire quand le personnage meurt
+    /// </summary>
+    void OnDeath()
+    {
+        //A FAIRE PTDR
+    }
+}
